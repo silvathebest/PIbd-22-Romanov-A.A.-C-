ea4e406ba1f2960f240f7f02bf3e9b0fcb4dfea1
silvathebest <silverchat228@gmail.com>
2020-11-29 18:42:02 +0400

    lab_6_update_commit

---
+++ WindowsFormsCrane/WindowsFormsCrane/ParkingNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsCrane
+{
+    public class ParkingNotFoundException : Exception
+    {
+        public ParkingNotFoundException(int i) : base("Не найден автомобиль по месту " + i)
+        { }
+    }
+
+}
---
+++ WindowsFormsCrane/WindowsFormsCrane/ParkingOverflowException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsCrane
+{
+    public class ParkingOverflowException : Exception
+    {
+        public ParkingOverflowException() : base("На парковке нет свободных мест")
+        { }
+    }
+}
